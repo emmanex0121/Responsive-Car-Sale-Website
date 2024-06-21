a865f2054e976ec160bc1db33e9b3fed205f4872
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-06-21 16:24:38 +0000

    finished design for normal pc screen

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,3 +3,10 @@ const search = document.querySelector('.search-box');
 document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active');
 };
+
+// Header
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
