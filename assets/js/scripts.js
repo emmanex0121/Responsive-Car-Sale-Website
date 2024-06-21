a09ef51914654e0a2fa195ad11cf3b22c8f7a857
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-06-21 19:20:43 +0000

    Responsive design for all screens completed

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,26 @@
-const search = document.querySelector('.search-box');
+const search = document.querySelector(".search-box");
 
-document.querySelector('#search-icon').onclick = () => {
-    search.classList.toggle('active');
+document.querySelector("#search-icon").onclick = () => {
+  search.classList.toggle("active");
+  menu.classList.remove("active");
+};
+
+const menu = document.querySelector(".navbar");
+
+document.querySelector("#menu-icon").onclick = () => {
+  menu.classList.toggle("active");
+  search.classList.remove("active");
+};
+
+// Hide menu And search box on scroll
+window.onscroll = () => {
+  search.classList.remove("active");
+  menu.classList.remove("active");
 };
 
 // Header
-let header = document.querySelector('header');
+const header = document.querySelector("header");
 
-window.addEventListener('scroll', () => {
-    header.classList.toggle('shadow', window.scrollY > 0);
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
 });
